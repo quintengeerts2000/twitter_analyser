6369214267e25f558580427789afbb9f1c8accb9
Quinten Geerts <s0183249@ad.ua.ac.be>
2021-02-01 13:59:16 +0100

    setting up the directory...

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='twitter_analyser_quintengeerts',
+    version='0.0.1',
+    author='Quinten Geerts',
+    author_email='quinten.geerts@gmail.com',
+    packages=setuptools.find_packages()
+)
